0190f8508155b4e1dd502f07d0d3d5531ce95130
Herohe <1071645574@qq.com>
2021-05-12 15:43:55 +0800

    update src/main/java/com/herohe/git/GitTest.java.

--- src/main/java/com/herohe/git/GitTest.java
+++ src/main/java/com/herohe/git/GitTest.java
@@ -3,5 +3,6 @@ package com.herohe.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
